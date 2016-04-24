e16023935db16e1abafe19dd125ffaeb6c657cc8
Malte Schmitz <malte@schmitz-sh.de>
2016-04-24 20:28:14 +0000

    Fix mixer init and change pin

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ pygame.init()
 class ToneSound(pygame.mixer.Sound):
     def __init__(self, frequency, volume):
         self.frequency = frequency
-        pygame.mixer.Sound.__init__(self, self.build_samples())
+	pygame.mixer.Sound.__init__(self, buffer=self.build_samples())
         self.set_volume(volume)
 
     def build_samples(self):
@@ -54,8 +54,8 @@ def decoder_thread():
 
 tone_obj = ToneSound(frequency = 800, volume = .5)
 
-pin = 7
-GPIO.setmode(GPIO.BOARD)
+pin = 21
+GPIO.setmode(GPIO.BCM)
 GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 DOT = "."
