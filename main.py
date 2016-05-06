c48eda6ee47d0302296c3dc598cbec40e2c4707a
Malte Schmitz <malte@schmitz-sh.de>
2016-05-06 17:19:43 +0200

    Make speed adjustable

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 
 import pygame
 import time
+import math
 from RPi import GPIO
 from array import array
 from pygame.locals import *
@@ -10,12 +11,6 @@ import Tkinter as tk
 
 pygame.mixer.pre_init(44100, -16, 1, 1024)
 pygame.init()
-
-bpm = 30
-dit_length = 6.0 / bpm
-dit_dash = 2*dit_length
-letter_break = 3*dit_length
-word_break = 10*dit_length
 
 class ToneSound(pygame.mixer.Sound):
     def __init__(self, frequency, volume):
@@ -171,17 +166,33 @@ class FullScreenApp(object):
         self.root = root
 
         self.tone_obj = ToneSound(frequency = 800, volume = .5)
-
         self.start_pos = root.winfo_screenwidth() - 200
+        
+        self.cpm = tk.StringVar()
+        self.cpm.trace("w", self.cpm_changed)
+        
+        self.cpm.set(30)
 
         self.canvas = tk.Canvas(height=100, relief=tk.FLAT, highlightthickness=0, background="black")
         self.canvas.grid(sticky=tk.W+tk.E, row=0, columnspan=2)
         
-        self.text = tk.Text(root, relief=tk.FLAT, borderwidth=10, highlightthickness=0, font=("DejaVu Sans Mono",50), width=1, height=1, wrap=tk.WORD)
+        self.text = tk.Text(self.root, relief=tk.FLAT, borderwidth=10, highlightthickness=0, font=("DejaVu Sans Mono",50), width=1, height=1, wrap=tk.WORD)
         self.text.grid(sticky=tk.W+tk.E+tk.N+tk.S, row=1)
 
         self.tree = DecodeTree(root)
-        self.tree.canvas.grid(sticky=tk.N+tk.S, row=1, column=1)
+        self.tree.canvas.grid(sticky=tk.N+tk.S, row=1, column=1, rowspan=2)
+        
+        self.foot = tk.Frame(root, borderwidth=10, background="white")
+        self.foot.grid(sticky=tk.N+tk.W+tk.E+tk.S, row=2, column=0)
+        
+        tk.Label(self.foot, text="Speed:", font=("DejaVu Sans Mono",24), background="white").pack(side=tk.LEFT)
+        self.cpmEdit = tk.Entry(self.foot, font=("DejaVu Sans Mono",24), width=4, justify=tk.RIGHT, textvar=self.cpm, highlightthickness=0, relief=tk.FLAT)
+        self.cpmEdit.pack(side=tk.LEFT)
+        tk.Label(self.foot, text=" CpM ", font=("DejaVu Sans Mono",24), background="white").pack(side=tk.LEFT)
+        tk.Button(self.foot, text="+", command=self.cpm_plus, font=("DejaVu Sans Mono",24)).pack(side=tk.LEFT)
+        tk.Label(self.foot, text=" ", font=("DejaVu Sans Mono",24), background="white").pack(side=tk.LEFT)
+        tk.Button(self.foot, text="−", command=self.cpm_minus, font=("DejaVu Sans Mono",24)).pack(side=tk.LEFT)
+        
         
         self.root.grid_columnconfigure(0, weight=1)
         self.root.grid_rowconfigure(1, weight=1)
@@ -195,6 +206,41 @@ class FullScreenApp(object):
         self.key_up_time = 0
         self.new_word = False
         self.read_input()
+        
+    def cpm_changed(self, *args):
+        value = self.cpm.get()
+        valid = len(value) < 4
+        if valid:
+            try:
+                if value:
+                    cpm = int(self.cpm.get())
+                    if cpm < 0:
+                        valid = False
+                    elif cpm > 0:
+                        self.cpm_update(cpm)
+            except ValueError:
+                valid = False
+        if valid:
+            self.cpmOld = self.cpm.get()
+        else:
+            self.cpm.set(self.cpmOld)
+        
+    def cpm_update(self, cpm):
+        self.cpmValue = cpm
+        self.dit_length = 6.0 / self.cpmValue
+        self.dit_dash = 2 * self.dit_length
+        self.letter_break = 3 * self.dit_length
+        self.word_break = 7 * self.dit_length
+        
+    def cpm_plus(self, *args):
+        cpm = int(math.floor(self.cpmValue / 5.0)) * 5
+        if (cpm < 995):
+            self.cpm.set(cpm + 5)
+        
+    def cpm_minus(self, *args):
+        cpm = int(math.ceil(self.cpmValue / 5.0)) * 5
+        if (cpm > 5):
+            self.cpm.set(cpm - 5)
 
     def create_new_line(self):
         self.last_line = self.canvas.create_line(self.start_pos, 50,
@@ -216,19 +262,19 @@ class FullScreenApp(object):
 
     # Call this method as long as the key is up
     def decode_is_up(self, key_up_length):
-        if key_up_length >= letter_break:
+        if key_up_length >= self.letter_break:
             if self.tree.is_char_available():
                 self.text.insert(tk.INSERT, self.tree.current_char())
                 self.new_word = True
                 self.tree.reset()
-        if key_up_length >= word_break:
+        if key_up_length >= self.word_break:
             if self.new_word:
                 self.text.insert(tk.INSERT, " ")
                 self.new_word = False
 
     # Call this method if the key is released
     def decode_was_down(self, key_down_length):
-        if key_down_length > dit_dash:
+        if key_down_length > self.dit_dash:
             self.tree.dash()
         else:
             self.tree.dot()
