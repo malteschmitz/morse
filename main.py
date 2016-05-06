d4ae6883bc55d9e9ba22ce5710add09d219335d3
Malte Schmitz <malte@schmitz-sh.de>
2016-05-06 18:35:55 +0200

    Add more characters
    - Remove umlauts. They are not part of the international standard.
    - Color unused nodes in the tree gray.
    - Improve support for empty tree elements

--- main.py
+++ main.py
@@ -54,43 +54,79 @@ TREE_INIT_Y = 450
 TREE_DRAW = "black"
 TREE_SELECTED_DRAW = "red"
 TREE_FILL = "light blue"
+TREE_EMPTY_FILL = "light gray"
 TREE_SELECTED_FILL = "light pink"
 
 class DecodeTree:
     def __init__(self, parent):
-        self.canvas = tk.Canvas(parent, width=500, height=500, background="white", highlightthickness=0)
+        self.canvas = tk.Canvas(parent, width=560, height=200, background="white", highlightthickness=0)
 
         self.root_node = Node("",
             Node("E",
                 Node("I",
                     Node("S",
-                        Node("H", Node("5"), Node("4")),
-                        Node("V", None, Node("3"))),
+                        Node("H",
+                            Node("5"),
+                            Node("4")),
+                        Node("V",
+                        None,
+                        Node("3"))),
                     Node("U",
                         Node("F"),
-                        Node("Ü", None, Node("2")))),
+                        Node("",
+                            Node("",
+                                Node("?"),
+                                Node("-")),
+                            Node("2")))),
                 Node("A",
                     Node("R",
                         Node("L"),
-                        Node("Ä", Node("+"))),
+                        Node("",
+                            Node("+",
+                                None,
+                                Node(".")))),
                     Node("W",
-                        Node("P"),
-                        Node("J", None, Node("1"))))),
+                        Node("P",
+                            None,
+                            Node("",
+                                Node("@"))),
+                        Node("J",
+                            None,
+                            Node("1"))))),
             Node("T",
                 Node("N",
                     Node("D",
-                        Node("B", Node("6"), Node("=")),
-                        Node("X", Node("/"))),
+                        Node("B",
+                            Node("6",
+                                None,
+                                Node("-")),
+                            Node("=")),
+                        Node("X",
+                            Node("/"))),
                     Node("K",
-                        Node("C"),
-                        Node("Y"))),
+                        Node("C",
+                            None,
+                            Node("",
+                                Node(";"))),
+                        Node("Y",
+                            Node("(",
+                                None,
+                                Node(")"))))),
                 Node("M",
                     Node("G",
-                        Node("Z", Node("7")),
+                        Node("Z",
+                            Node("7"),
+                            Node("",
+                                None,
+                                Node(","))),
                         Node("Q")),
                     Node("O",
-                        Node("Ö", Node("8")),
-                        Node("CH", Node("9"), Node("0"))))))
+                        Node("",
+                            Node("8",
+                                Node(":"))),
+                        Node("",
+                            Node("9"),
+                            Node("0"))))))
 
         self.plot_tree(self.root_node, 1, TREE_INIT_Y)
 
@@ -102,16 +138,19 @@ class DecodeTree:
         self.mark()
 
     def clear_tree(self, node):
-        if node.oval != None:
-            self.canvas.itemconfig(node.oval, fill=TREE_FILL)
+        if node.oval:
+            if node.value:
+                self.canvas.itemconfig(node.oval, fill=TREE_FILL)
+            else:
+                self.canvas.itemconfig(node.oval, fill=TREE_EMPTY_FILL)
             self.canvas.itemconfig(node.oval, outline=TREE_DRAW)
             self.canvas.itemconfig(node.oval, width=TREE_LINE_WIDTH)
-        if node.line != None:
+        if node.line:
             self.canvas.itemconfig(node.line, fill=TREE_DRAW)
             self.canvas.itemconfig(node.line, width=TREE_LINE_WIDTH)
-        if node.dash != None:
+        if node.dash:
             self.clear_tree(node.dash)
-        if node.dot != None:
+        if node.dot:
             self.clear_tree(node.dot)
 
     def clear(self):
@@ -119,24 +158,24 @@ class DecodeTree:
         self.clear_tree(self.root_node)
     
     def mark(self):
-        if self.current_node != None:
-            if self.current_node.oval != None:
+        if self.current_node:
+            if self.current_node.oval:
                 self.canvas.itemconfig(self.current_node.oval, fill=TREE_SELECTED_FILL)
                 self.canvas.itemconfig(self.current_node.oval, outline=TREE_SELECTED_DRAW)
                 self.canvas.itemconfig(self.current_node.oval, width=TREE_SELECTED_LINE_WITH)
-            if self.current_node.line != None:
+            if self.current_node.line:
                 self.canvas.itemconfig(self.current_node.line, fill=TREE_SELECTED_DRAW)
                 self.canvas.itemconfig(self.current_node.line, width=TREE_SELECTED_LINE_WITH)
 
     def dash(self):
-        if self.current_node != None and self.current_node.dash != None:
+        if self.current_node and self.current_node.dash:
             self.current_node = self.current_node.dash
             self.mark()
         else:
             self.clear()
 
     def dot(self):
-        if self.current_node != None and self.current_node.dot != None:
+        if self.current_node and self.current_node.dot:
             self.current_node = self.current_node.dot
             self.mark()
         else:
@@ -146,20 +185,21 @@ class DecodeTree:
         return self.current_node != self.root_node
 
     def current_char(self):
-        if self.current_node != None and self.current_node.value != "":
+        if self.current_node and self.current_node.value:
             return self.current_node.value
         else:
-            return "?"
+            return None
 
     def plot_tree(self, node, level, y):
-        if node.dot != None:
-            node.dot.line = self.canvas.create_line(TREE_LEFT_OFFSET + level * TREE_LEVEL_DEPTH + TREE_DIAMETER/2, y + TREE_DIAMETER/2, TREE_LEFT_OFFSET + level * TREE_LEVEL_DEPTH + TREE_LEVEL_DEPTH + TREE_DIAMETER/2, y + TREE_SIBLING_DISTANCE(level)/2 + TREE_DIAMETER/2, width=TREE_LINE_WIDTH, dash=(3,3))
+        if node.dot:
+            node.dot.line = self.canvas.create_line(TREE_LEFT_OFFSET + level * TREE_LEVEL_DEPTH + TREE_DIAMETER/2, y + TREE_DIAMETER/2, TREE_LEFT_OFFSET + level * TREE_LEVEL_DEPTH + TREE_LEVEL_DEPTH + TREE_DIAMETER/2, y + TREE_SIBLING_DISTANCE(level)/2 + TREE_DIAMETER/2, dash=(3,3))
             self.plot_tree(node.dot, level + 1, y + TREE_SIBLING_DISTANCE(level)/2)
-        if node.dash != None:
-            node.dash.line = self.canvas.create_line(TREE_LEFT_OFFSET + level * TREE_LEVEL_DEPTH + TREE_DIAMETER/2, y + TREE_DIAMETER/2, TREE_LEFT_OFFSET + level * TREE_LEVEL_DEPTH + TREE_LEVEL_DEPTH + TREE_DIAMETER/2, y - TREE_SIBLING_DISTANCE(level)/2 + TREE_DIAMETER/2, width=TREE_LINE_WIDTH, dash=(9,3))
+        if node.dash:
+            node.dash.line = self.canvas.create_line(TREE_LEFT_OFFSET + level * TREE_LEVEL_DEPTH + TREE_DIAMETER/2, y + TREE_DIAMETER/2, TREE_LEFT_OFFSET + level * TREE_LEVEL_DEPTH + TREE_LEVEL_DEPTH + TREE_DIAMETER/2, y - TREE_SIBLING_DISTANCE(level)/2 + TREE_DIAMETER/2, dash=(9,3))
             self.plot_tree(node.dash, level + 1, y - TREE_SIBLING_DISTANCE(level)/2)
-        node.oval = self.canvas.create_oval(TREE_LEFT_OFFSET + level * TREE_LEVEL_DEPTH, y, TREE_LEFT_OFFSET + level * TREE_LEVEL_DEPTH + TREE_DIAMETER, y + TREE_DIAMETER, width=TREE_LINE_WIDTH, fill="light blue")
-        self.canvas.create_text(TREE_LEFT_OFFSET + level * TREE_LEVEL_DEPTH + TREE_DIAMETER / 2, y + TREE_DIAMETER / 2, text=node.value, font=("DejaVu Sans Mono",18))  
+        node.oval = self.canvas.create_oval(TREE_LEFT_OFFSET + level * TREE_LEVEL_DEPTH, y, TREE_LEFT_OFFSET + level * TREE_LEVEL_DEPTH + TREE_DIAMETER, y + TREE_DIAMETER)
+        if node.value:
+            self.canvas.create_text(TREE_LEFT_OFFSET + level * TREE_LEVEL_DEPTH + TREE_DIAMETER / 2, y + TREE_DIAMETER / 2, text=node.value, font=("DejaVu Sans Mono",16))  
 
 class FullScreenApp(object):
     def __init__(self, root):
@@ -266,7 +306,10 @@ class FullScreenApp(object):
     def decode_is_up(self, key_up_length):
         if key_up_length >= self.letter_break:
             if self.tree.is_char_available():
-                self.text.insert(tk.INSERT, self.tree.current_char())
+                char = self.tree.current_char()
+                if not char:
+                    char = "�"
+                self.text.insert(tk.INSERT, char)
                 self.new_word = True
                 self.tree.reset()
         if key_up_length >= self.word_break:
